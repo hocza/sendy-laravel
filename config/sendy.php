6e6a0a5d48a2401a25a336de33c59c00e774e911
Jozsef Hocza <jozsef@hocza.com>
2015-03-17 21:29:53 +0100

    some modification

--- config/sendy.php
+++ config/sendy.php
@@ -1,14 +1,11 @@
 <?php
 return array(
-    /*
-    |--------------------------------------------------------------------------
-    | API Secret Key
-    |--------------------------------------------------------------------------
-    |
-    | The api secret key provided by Mailchimp. If you don't know your API key
-    | you can find out how to get it here:
-    | "http://kb.mailchimp.com/article/where-can-i-find-my-api-key/"
-    |
-    */
-    'apikey' => 'your-key-here',
+    //Default list_id
+    'list_id' => '',
+
+    //installation url :)
+    'installation_url' => '',
+
+    //and the API key... :)
+    'api_key' => ''
 );
