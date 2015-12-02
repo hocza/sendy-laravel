54694117b428ecf1151c01bf87aa80f4900292db
Alison <alisonmonteiro.10@gmail.com>
2015-12-02 00:08:56 -0300

    Define some tests

---
+++ tests/SendyTest.php
@@ -0,0 +1,21 @@
+<?php namespace Hocza\Tests\Sendy;
+
+use Hocza\Sendy;
+use PHPUnit_Framework_TestCase;
+
+class SendyTest extends PHPUnit_Framework_TestCase
+{
+    public function testSimpleSubscribe() {}
+
+    public function testSubscribeASubscriberThatAlreadyExists() {}
+
+    public function testCheckStatus() {}
+
+    public function testTransferSubscriberForAnotherList() {}
+
+    public function testSimpleUnsubscribe() {}
+
+    public function testUnsubscribeASubscriberThatNotExists() {}
+
+    public function testGetAccount() {}
+}
